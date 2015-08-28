fd0734be5b6def872a50bade516583bd68ffc911
g_ryotaro <goto@basicinc.jp>
2015-08-29 07:41:31 +0900

    一旦

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
   # def configure_permitted_parameters
   #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :single, :wife_name, :wife_email, :wedding_day, {:wife_image => [:wife_image_file_name, :wife_image_content_type, :wife_image_file_size, :wife_image_updated_at, :wife_image_fingerprint]}, {:family_image =>[ :wife_image_file_name, :wife_image_content_type, :wife_image_file_size, :wife_image_updated_at, :wife_image_fingerprint]}) }
   # end
+  def after_sign_in_path_for(resource)
+    check_path
+  end
+
+  def after_sign_out_path_for(resource)
+    admin_root_path
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
 	def index
 
 	end
+
+  def check
+    if current_user.present?
+    end
+  end
 end
