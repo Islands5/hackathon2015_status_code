8182fda0d80e4f3a561910561adac5925404c33f
g_ryotaro <goto@basicinc.jp>
2015-08-29 10:49:22 +0900

    チェックページのロジックをさらに追加'

--- app/controllers/uwaki_controller.rb
+++ app/controllers/uwaki_controller.rb
@@ -1,5 +1,24 @@
 class UwakiController < ApplicationController
 	def index
-	
+    if params[:question] = "true"
+      # 奥さんとホテルなう...
+    end
+    lat = params[:geo][:lat]
+    lon = params[:geo][:lon]
+
+    address = search_address(lat, lon)
 	end
+
+  def search_address(lat, lon)
+    url = 'http://usoinfo.if.land.to/osmtool/latlon2addr.php?ipt=csv&latcsv=' + lat + '&loncsv=' + lon
+    charset = nil
+    html = open(url) do |f|
+        charset = f.charset # 文字種別を取得
+          f.read # htmlを読み込んで変数htmlに渡す
+    end
+
+    # htmlをパース(解析)してオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    address = doc.xpath('//*[@id="osmtoolbox"]/div[5]/b').text
+  end
 end
